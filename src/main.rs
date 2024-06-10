4ba80cf95b3e9b41213201b696cc0e82b4e10ce5
sihala <ssihala@gmail.com>
2024-06-09 20:14:53 -0400

    Add rom name, speed arguments

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,25 @@
 mod chip8;
 mod interface;
 
+
 use chip8::Chip8;
 use crate::interface::Interface;
 
 
 fn main() -> Result<(), String>{
+    let mut args = std::env::args();    
+    let rom_name = args.nth(1).unwrap();
+    let rom_folder_path = String::from("./roms/");
+    let rom_path = rom_folder_path+&rom_name;
+    let cycle_speed_arg = args.next().unwrap();
+    let cycle_speed :i32 = cycle_speed_arg.parse().expect("Can't parse cycle speed, enter an integer");
 
-    let mut chip = Chip8::new(500);
-    chip.load_rom(&String::from("pong.rom"));
-    // chip.load_rom(&String::from("2-ibm-logo.ch8"));
-    // chip.load_rom(&String::from("1-chip8-logo.ch8"));
+    let mut chip = Chip8::new(cycle_speed);
+    chip.load_rom(&rom_path);
     chip.load_font();
 
-
     let interface = Interface::new(String::from("RUST-8"), 64, 32, 25);
-    
-    
-    
+       
     let _ = interface.window_loop(&mut chip);
 
     Ok(())
